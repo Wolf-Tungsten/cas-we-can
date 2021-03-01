a7930b189613e678fc115693720660cf76990911
R-shrug <213183580@seu.edu.cn>
2021-03-01 22:27:43 +0800

    除去多余的log

--- middleware/platform.js
+++ middleware/platform.js
@@ -1,6 +1,5 @@
 module.exports = function () {
   return async (ctx, next) => {
-    console.log(ctx.request.headers['user-agent'].toLowerCase())
     ctx.inWechat = (ctx.request.headers['user-agent']).toLowerCase().includes('micromessenger')
     await next()
   }
